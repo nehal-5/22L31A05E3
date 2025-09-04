9f4714803c2f981bae57596701114de3646bc3e0
nehal-5 <nehalfathema08@gmail.com>
2025-09-04 11:08:40 +0530

    Logging Middleware

--- backend/loggingMiddleware.js
+++ backend/loggingMiddleware.js
@@ -1,1 +1,19 @@
-console.log("It's working");
+const axios = require("axios");
+
+const LOG_URL = "http://20.244.56.144/evaluation-service/logs";
+
+async function LOG(stack, level, pkg, message) {
+  try {
+    await axios.post(LOG_URL, {
+      stack,
+      level,
+      package: pkg,
+      message,
+    });
+    console.log(`[${level}] (${pkg}) ${message}`);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+
+module.exports = { LOG };
